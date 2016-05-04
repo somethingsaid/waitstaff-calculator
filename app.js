0428ebf1af21d45e5bd23018549a7e2e9170fdeb
somethingsaid <kwong.billy@gmail.com>
2016-05-04 11:25:36 -0400

    Fixed view routing and controllers

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ angular.module('myApp', ['ngRoute'])
     })
     .when('/new_meal', {
         templateUrl : 'new_meal.html',
-        controller : 'mealCtrl'
+        controller : 'waitstaffController'
     })
     .when('/my_earnings', {
         templateUrl: 'my_earnings.html',
@@ -25,59 +25,61 @@ angular.module('myApp', ['ngRoute'])
 .controller('homeCtrl', ['$scope', function($scope) {
     //empty for now
 }])
-.controller('mealCtrl', ['$scope', function($scope) {
-    //empty for now
-}])
-.controller('earningsCtrl', ['$scope', function($scope) {
-    //empty for now
-}])
-.controller('waitstaffController', function($scope) {
-	// Init/Reset total earnings data
-    $scope.reset = function() {
-		$scope.data = {
-			/* Meal data */
-			mealPrice: null,
-			mealTaxPct: null,
-			mealTipPct: null,
-			/* Calculated meal data */
-			mealSubTotal: 0,
-			mealTipDollar: 0,
-			mealTotal: 0,
-			/* Aggregate data */
-		    mealCount: 0,
-			tipTotal: 0,
-			avgTip: 0,
-			submitted: false
-		};
-	}
-	$scope.reset();
-
+.controller('waitstaffController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.mealData = {
+        //Meal Data
+        mealPrice: 0,
+        mealTaxPct: 0,
+        mealTipPct: 0,
+        //Calculated meal data
+        mealSubTotal: 0,
+        mealTipDollar: 0,
+        mealTotal: 0
+    }
+    $rootScope.earningsData = $rootScope.earningsData || {
+            /* Earnings data */
+            mealCount: 0,
+            tipTotal: 0,
+            avgTip: 0
+    };
     // Submit meal details
     $scope.submit = function() {
-    	$scope.data.submitted = true;
-    	if ($scope.mealDetails.$valid) {
-    		console.log("Valid!");
-    		/* Calculated meal data */
-    		$scope.data.mealSubTotal = ($scope.data.mealPrice * (($scope.data.mealTaxPct / 100.00) + 1)).toFixed(2);
-    		$scope.data.mealTipDollar = (($scope.data.mealTipPct / 100) * $scope.data.mealPrice).toFixed(2);
-    		$scope.data.mealTotal = (($scope.data.mealSubTotal * 1) + ($scope.data.mealTipDollar * 1)).toFixed(2);
-    		/* Aggregate data */
-    		$scope.data.mealCount++;
-    		$scope.data.tipTotal = (($scope.data.tipTotal * 1) + ($scope.data.mealTipDollar * 1)).toFixed(2);
-    		$scope.data.avgTip = ($scope.data.tipTotal / $scope.data.mealCount).toFixed(2);
-    		/* Null out meal details in input */
-    		$scope.clear();
-    	}
-    	else {
-    		console.log("Missing data");
-    	}
+        $scope.mealData.submitted = true;
+        if ($scope.mealDetails.$valid) {
+            console.log("Valid!");
+            /* Calculated meal data */
+            $scope.mealData.mealSubTotal = ($scope.mealData.mealPrice * (($scope.mealData.mealTaxPct / 100.00) + 1)).toFixed(2);
+            $scope.mealData.mealTipDollar = (($scope.mealData.mealTipPct / 100) * $scope.mealData.mealPrice).toFixed(2);
+            $scope.mealData.mealTotal = (($scope.mealData.mealSubTotal * 1) + ($scope.mealData.mealTipDollar * 1)).toFixed(2);
+            /* Earnings data */
+            $rootScope.earningsData.mealCount++;
+            $rootScope.earningsData.tipTotal = (($rootScope.earningsData.tipTotal * 1) + ($scope.mealData.mealTipDollar * 1)).toFixed(2);
+            $rootScope.earningsData.avgTip = ($rootScope.earningsData.tipTotal / $rootScope.earningsData.mealCount).toFixed(2);
+            console.log('Meal #: ' + $rootScope.earningsData.mealCount + '; Tip Total: ' + $rootScope.earningsData.tipTotal + '; AvgTip: ' + $rootScope.earningsData.avgTip);
+            /* Null out meal details in input */
+            $scope.clear();
+        }
+        else {
+            console.log("Missing data");
+        }
     }
 
     // Reset meal details
     $scope.clear = function() {
-    	$scope.data.mealPrice = null;
-    	$scope.data.mealTaxPct = null;
-    	$scope.data.mealTipPct = null;
-    	$scope.data.submitted = false;
+        $scope.mealData.mealPrice = null;
+        $scope.mealData.mealTaxPct = null;
+        $scope.mealData.mealTipPct = null;
+        $scope.mealData.submitted = false;
+    };
+}])
+.controller('earningsCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+    // Init/Reset total earnings data
+    $scope.reset = function() {
+        $rootScope.earningsData = {
+            /* Earnings data */
+            mealCount: 0,
+            tipTotal: 0,
+            avgTip: 0
+        };
     }
-});
+}]);
